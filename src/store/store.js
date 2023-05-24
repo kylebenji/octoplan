ebfaf04015acc5d265be3161b3e42f2f3c802230
kylebenji <127806283+kylebenji@users.noreply.github.com>
2023-05-24 16:18:45 -0700

    clarifying comments on state

--- src/store/store.js
+++ src/store/store.js
@@ -19,7 +19,7 @@ const store = configureStore({
 store.subscribe(() => {
   const state = store.getState();
   saveState({
-    todos: { list: state.todos.list, idCounter: state.todos.idCounter },
+    todos: { list: state.todos.list, idCounter: state.todos.idCounter }, //only adding parts so that we load a new date and reset the active id each time we load
     filters: store.getState().filters,
   });
 });
